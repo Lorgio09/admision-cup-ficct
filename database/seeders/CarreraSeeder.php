86db9b9df3d7115d73a141bbae0c0caf57fb042a
Lorgio09 <lorgio.choque09@gmail.com>
2026-05-30 23:29:40 -0400

    Inicializar carreras con cupo 0 en el seeder

--- database/seeders/CarreraSeeder.php
+++ database/seeders/CarreraSeeder.php
@@ -10,18 +10,16 @@ class CarreraSeeder extends Seeder
     public function run(): void
     {
         $carreras = [
-            ['codigo' => '187-3', 'nombre' => 'Ingeniería en Sistemas'],
-            ['codigo' => '187-4', 'nombre' => 'Ingeniería Informática'],
-            ['codigo' => '187-5', 'nombre' => 'Ingeniería en Redes y Telecomunicaciones'],
-            ['codigo' => '187-6', 'nombre' => 'Ingeniería en Robotica']
+            // Inicializamos el cupo en 0 hasta que el administrador lo asigne en el sistema
+            ['codigo' => '187-3', 'nombre' => 'Ingeniería en Sistemas', 'cupo' => 0],
+            ['codigo' => '187-4', 'nombre' => 'Ingeniería Informática', 'cupo' => 0],
+            ['codigo' => '187-5', 'nombre' => 'Ingeniería en Redes y Telecomunicaciones', 'cupo' => 0]
         ];
 
         foreach ($carreras as $carrera) {
-            // Ahora buscamos por 'codigo' para evitar duplicados. 
-            // Si no encuentra el código, crea la carrera con su nombre.
             Carrera::firstOrCreate(
                 ['codigo' => $carrera['codigo']], 
-                ['nombre' => $carrera['nombre']]
+                $carrera
             );
         }
     }
